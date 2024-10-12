effd5ffdbaee4124a00f988c70b8ae9ee74f7235
Herton Cabral Mabongue <HERTON@APPLEs-MacBook-Pro.local>
2024-10-12 23:25:53 +0200

    first changes

---
+++ src/GameState.py
@@ -0,0 +1,24 @@
+"""
+This class holds the current Game State.
+
+"""
+
+
+class GameState:
+    def __int__(self):
+        self.initial_board = [
+            ['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+            ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['--', '--', '--', '--', '--', '--', '--', '--'],
+            ['wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+            ['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR']
+        ]
+
+        self.white_turn = True
+        self.move_log = []
+
+
+
